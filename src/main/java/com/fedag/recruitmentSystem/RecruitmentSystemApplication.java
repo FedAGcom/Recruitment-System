6b807d71e209a0699bd1275f5681c3a7ac4d961d
fedor <fedorcvetkov7@gmail.com>
2022-05-30 11:37:11 +0300

    Initial commit

---
+++ src/main/java/com/fedag/recruitmentSystem/RecruitmentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.fedag.recruitmentSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitmentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecruitmentSystemApplication.class, args);
+	}
+
+}
