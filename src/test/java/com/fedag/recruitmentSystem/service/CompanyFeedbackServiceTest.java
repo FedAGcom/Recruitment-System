a337191cca6d59849dfc26bfd351affcd3c0b9ed
Nick Amin <daymne@mail.ru>
2022-06-02 11:36:57 +0300

    Add test.

---
+++ src/test/java/com/fedag/recruitmentSystem/service/CompanyFeedbackServiceTest.java
@@ -0,0 +1,58 @@
+package com.fedag.recruitmentSystem.service;
+
+import com.fedag.recruitmentSystem.model.CompanyFeedBack;
+import com.fedag.recruitmentSystem.repository.CompanyFeedbackRepository;
+import com.fedag.recruitmentSystem.service.impl.CompanyFeedbackServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.Pageable;
+
+import java.util.Optional;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CompanyFeedbackServiceTest {
+    @Mock
+    CompanyFeedbackRepository companyFeedbackRepository;
+
+    @InjectMocks
+    CompanyFeedbackServiceImpl companyFeedbackService;
+
+    @Test
+    void testGetAllCompanyFeedbacks() {
+        companyFeedbackService.getAllCompanyFeedbacks();
+        verify(companyFeedbackRepository).findAll();
+    }
+
+    @Test
+    void testGetAllCompanyFeedbacksPageable() {
+        Pageable pageable = Mockito.any(Pageable.class);
+        companyFeedbackService.getAllCompanyFeedbacks(pageable);
+        verify(companyFeedbackRepository).findAll(pageable);
+    }
+
+    @Test
+    void testFindById(){
+        when(companyFeedbackRepository.findById(anyLong())).thenReturn(Optional.of(new CompanyFeedBack()));
+        companyFeedbackService.findById(anyLong());
+        verify(companyFeedbackRepository).findById(anyLong());
+    }
+
+    @Test
+    void testSave() {
+        CompanyFeedBack companyFeedBack = new CompanyFeedBack();
+        companyFeedbackService.save(companyFeedBack);
+        verify(companyFeedbackRepository).save(companyFeedBack);
+    }
+
+    @Test
+    void testDeleteById() {
+        companyFeedbackService.deleteById(anyLong());
+        verify(companyFeedbackRepository).deleteById(anyLong());
+    }
+}
