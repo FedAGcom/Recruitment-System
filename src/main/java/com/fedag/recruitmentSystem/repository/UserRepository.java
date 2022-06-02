33d8c0d100edba89479c8945999c63d3b54b1fa7
grits <gritskovvvv@gmail.com>
2022-06-02 14:06:49 +0500

    repo fix

--- src/main/java/com/fedag/recruitmentSystem/repository/UserRepository.java
+++ src/main/java/com/fedag/recruitmentSystem/repository/UserRepository.java
@@ -2,6 +2,8 @@ package com.fedag.recruitmentSystem.repository;
 
 import com.fedag.recruitmentSystem.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
