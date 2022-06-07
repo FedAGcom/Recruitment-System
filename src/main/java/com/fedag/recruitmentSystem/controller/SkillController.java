2e4102f77222a6fb43a89e8121d6d988084b9484
gritskovvvv <gritskovvvv@gmail.com>
2022-06-07 20:19:19 +0500

    fix @PutMapping, dto's

--- src/main/java/com/fedag/recruitmentSystem/controller/SkillController.java
+++ src/main/java/com/fedag/recruitmentSystem/controller/SkillController.java
@@ -69,19 +69,6 @@ public class SkillController {
     skillService.deleteById(id);
   }
 
-  @Operation(summary = "Добавление ключевого навыка")
-  @ApiResponses(value = {
-          @ApiResponse(responseCode = "201", description = "Навык добавлен",
-                  content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)}),
-          @ApiResponse(responseCode = "500", description = "Внутренняя ошибка сервера",
-                  content = {@Content(mediaType = MediaType.APPLICATION_JSON_VALUE)})
-  })
-  @PutMapping("/{id}")
-  public void addVacancy(@PathVariable Long id, @RequestBody SkillRequest skillRequest) {
-    skillRequest.setId(id);
-    skillService.save(skillRequest);
-  }
-
   @Operation(summary = "Изменение ключевого навыка")
   @ApiResponses(value = {
           @ApiResponse(responseCode = "200", description = "Навык изменен",
@@ -91,6 +78,7 @@ public class SkillController {
   })
   @PutMapping("/{id}")
   public void updateVacancy(@PathVariable Long id, @RequestBody SkillUpdateRequest skillUpdateRequest) {
+    skillUpdateRequest.setId(id);
     skillService.update(skillUpdateRequest);
   }
 }
