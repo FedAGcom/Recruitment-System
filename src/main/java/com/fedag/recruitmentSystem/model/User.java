180745ba6b038112a0ce19d7b7ded3628feafb23
Vladislav Gorbunov <wint-mil@mail.ru>
2022-06-10 11:39:54 +0300

    fix

--- src/main/java/com/fedag/recruitmentSystem/model/User.java
+++ src/main/java/com/fedag/recruitmentSystem/model/User.java
@@ -62,11 +62,11 @@ public class User {
     @OneToMany(mappedBy = "user")
     private List<Message> messageList;
 
-    @ManyToMany
-    @JoinTable(
-            name = "user_project_link",
-            joinColumns = @JoinColumn(name = "user_id"),
-            inverseJoinColumns = @JoinColumn(name = "project_id"))
-    private List<Project> projects;
+//    @ManyToMany
+//    @JoinTable(
+//            name = "user_project_link",
+//            joinColumns = @JoinColumn(name = "user_id"),
+//            inverseJoinColumns = @JoinColumn(name = "project_id"))
+//    private List<Project> projects;
 }
 
