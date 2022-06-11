46645f8e1ae1dcea92f07a036e652cc3a3a9928b
Vladislav Gorbunov <wint-mil@mail.ru>
2022-06-11 16:20:14 +0300

    working active link

--- src/main/java/com/fedag/recruitmentSystem/email/MailSendlerService.java
+++ src/main/java/com/fedag/recruitmentSystem/email/MailSendlerService.java
@@ -3,9 +3,15 @@ package com.fedag.recruitmentSystem.email;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.File;
 
 @Service
 @RequiredArgsConstructor
