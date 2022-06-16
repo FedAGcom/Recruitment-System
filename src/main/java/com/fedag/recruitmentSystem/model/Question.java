01919c69b89e17671ea7f27ded32fe05100baf5a
Nick Amin <daymne@mail.ru>
2022-06-16 14:40:04 +0300

    Fix bugs in Question.

--- src/main/java/com/fedag/recruitmentSystem/model/Question.java
+++ src/main/java/com/fedag/recruitmentSystem/model/Question.java
@@ -4,15 +4,10 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Id;
-
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class Question {
-
-    @Id
-    private String id;
 
     private String title;
 
